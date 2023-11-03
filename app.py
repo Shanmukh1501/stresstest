d9fbfd4fff8d9279ecbc0a62da666be126fe0a73
Shanmukh1501 <kshanmukha1501@gmail.com>
2023-11-04 03:49:47 +0530

    changed app.py

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ def start_cpu_stress():
 def start_ram_stress():
     try:
         # Stress RAM by allocating 6GB for 300 seconds
-        subprocess.Popen(["stress", "--vm", "1", "--vm-bytes", "3G", "--timeout", "300"])
-        return jsonify({'message': 'RAM stress started with 6GB for 300 seconds'}), 200
+        subprocess.Popen(["stress", "--vm", "1", "--vm-bytes", "4G", "--timeout", "300"])
+        return jsonify({'message': 'RAM stress started with 4GB for 300 seconds'}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
@@ -35,7 +35,7 @@ def start_disk_write_stress():
     try:
         # Write 3GB to disk for 480 seconds
         subprocess.Popen(["stress", "--hdd", "2", "--hdd-bytes", "3G", "--timeout", "480"])
-        return jsonify({'message': 'Disk write stress started with 1GB for 300 seconds'}), 200
+        return jsonify({'message': 'Disk write stress started with 3GB for 7 Minutes'}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
